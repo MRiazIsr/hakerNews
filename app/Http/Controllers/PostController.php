963be5bbed01c7664158f5c68811b91cd4e8b604
Mark Riazantsev <markriazantsev@ip-10-0-0-7.eu-west-1.compute.internal>
2022-03-12 21:03:52 +0200

    add Model add Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public static function get () {
+        $posts = Post::get();
+
+        if ($posts || empty($posts)) {
+            return 'POSTS ARE EMPTY';
+        } else {
+            $data = array (
+                'id' => $posts->id,
+                'content' => $posts->content
+            );
+            return json_encode($data);
+        }
+    }
+
+    public static function create (Request $request) {
+        //
+    }
+
+    public static function edit (Request $request) {
+        //
+    }
+
+    public static function delete (Request $request) {
+        //
+    }
+
+    private static function doCalculation ($someData){
+        //
+    }
+
+}
